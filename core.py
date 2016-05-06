fd4bc191107b28b6305840661b1c1316568050a8
udeos <ruzhitskiy.bogdan@gmail.com>
2016-05-06 12:29:47 +0300

    ThreadProcessor

--- core.py
+++ core.py
@@ -1,32 +1,28 @@
 from queue import Queue
 from threading import Thread
-from http import client
 
 
-class RequestProcessor:
+class ThreadProcessor:
 
     def __init__(self, callback, concurrency=1):
         self.callback = callback
         self.concurrency = concurrency
-        self.queue = Queue(self.concurrency)
-        self.daemons = []
+        self.queue = Queue(self.concurrency * 2)
 
-    def process(self, urls):
-        self.spawn_daemons()
-        for url in urls:
-            self.queue.put(url)
+    def process(self, items):
+        self._spawn_daemons()
+        for item in items:
+            self.queue.put(item)
         self.queue.join()
-        self.kill_daemons()
 
-    def spawn_daemons(self):
+    def _spawn_daemons(self):
         for i in range(self.concurrency):
             t = Thread(target=self.daemon_worker)
             t.daemon = True
             t.start()
-            self.daemons.append(t)
-
-    def kill_daemons(self):
-        pass
 
     def daemon_worker(self):
-        pass
+        while True:
+            item = self.queue.get()
+            self.callback(item)
+            self.queue.task_done()
--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from time import time
+
+from core import ThreadProcessor
+
+
 if __name__ == '__main__':
-    pass
+
+    def callback(response):
+        print(response)
+
+    t = time()
+    rp = ThreadProcessor(callback, 3)
+    rp.process(['http://ping.eu/'] * 100)
+    print(time() - t)
